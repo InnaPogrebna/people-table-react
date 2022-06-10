f0c45fc1c965df187baf983e3f34432bfe814833
Inna <inna_temis@ukr.net>
2022-06-10 15:29:37 +0300

    peopel-table_react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bulma/css/bulma.min.css';
+import {
+  Routes, Route, Navigate, Link,
+} from 'react-router-dom';
+import PeoplePage from './components/PeoplePage/PeoplePage';
+import HomePage from './components/HomePage/HomePage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header>
+      <nav className="navbar">
+        <Link className="navbar-item" to="/">Home page</Link>
+        <Link className="navbar-item" to="/people">People page</Link>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="people" element={<PeoplePage />}>
+        <Route path=":slug" element={<PeoplePage />} />
+      </Route>
+      <Route path="*" element={<p>Page not found</p>} />
+      <Route path="/home" element={<Navigate replace to="/" />} />
+    </Routes>
+  </div>
+);
 
 export default App;
